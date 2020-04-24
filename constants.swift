be6ea9e3e7407d6f377dac818ffa4a13b2ebb448
Jobson Mateus <44685592+JobsonMateusAlves@users.noreply.github.com>
2020-04-24 11:10:17 -0300

    Update constants.swift

--- constants.swift
+++ constants.swift
@@ -1,12 +1,15 @@
 // Declare constants with `let`.
 // A constant can appear anywhere a `var` statement can.
+
 let s = "string"
 print(s)            // string
 
 let n = 500000000
 print(n)            // 500000000
 
-let r = 3 ^ 20 / n
-print(r)            // 3
+let r = n / 100000000
+print(r)            // 5
+
+//r = 2  Cannot assign to value: 'r' is a 'let' constant
 
 // 'let' declarations require an initializer expression
